b82636836a7aa53cd8b9838aa235f73e5eff96d0
Amogh Sahasrabhojanee <amogh1773@gmail.com>
2022-06-02 11:51:31 +0530

    play around with compression

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const express = require("express");
+const zlib = require("zlib");
+
 const app = express();
 const port = 3002;
+const payload = "HelloWorld\n".repeat(1000);
 
 app.use((req, res, next) => {
   console.log(`${req.method} on ${req.path} ${Date.now()}`);
   next();
 });
 app.use(express.static("public"));
-app.use(express.json())
-app.get("/api/v1/config", (req, res) => {
-  const dummyConfigObj = {
-    "cdn-name": "https://gumlet.assettype.com/",
-    "publisher-id": 829,
-    "cdn-image": "gumlet.assettype.com",
-  };
-  return res.json(dummyConfigObj);
-});
-app.get("/", (req, res) => res.send("Hello"));
-app.post("/api/save-daily-data", (req, res) => {
-  console.log("req.body >> ", req.body);
-  res.status(200).send("ok");
-});
+app.use(express.json());
+app.get("/", (req, res) => res.send(payload));
+app.get("/gzip", (req, res, next) => {
+  zlib.gzip(payload, (err, encoded) => {
+    if (err) return next(err)
+    res.setHeader("Content-Type", "text/html")
+    res.setHeader("content-encoding", "gzip")
+    res.send(encoded)
+  })
+})
+app.get("/brotli", (req, res, next) => {
+  zlib.brotliCompress(payload, (err, encoded) => {
+    if (err) return next(err)
+    res.setHeader("Content-Type", "text/html")
+    res.setHeader("content-encoding", "br")
+    res.send(encoded)
+  })
+})
 app.all("*", (req, res) => res.status(404).send("Not Found"));
 
 app.listen(port, () => {
