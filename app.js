9f852a0e31762084e2618da3174d85f5e5dec8b1
Amogh Sahasrabhojanee <amogh1773@gmail.com>
2023-08-11 15:18:55 +0530

    data dump

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const payload = "HelloWorld\n".repeat(10);
+const fs = require("fs");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ limit: '100mb' }));
 
 app.use((req, res, next) => {
   console.log(
@@ -11,6 +15,18 @@ app.use((req, res, next) => {
 });
 app.use(express.static("public"));
 app.use(express.json());
+
+app.post("/json-dump", (req, res) => {
+  try {
+    fs.writeFileSync(
+      `./data-dump/${Date.now()}.json`,
+      JSON.stringify(req.body)
+    );
+    res.status(200).send("All OK");
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 // app.get("/", (req, res) => res.redirect(301, "/james_may_says_cheese.mp4"));
 app.get("/", (req, res) => res.status(200).send(payload));
